7117119e42bf18b9a4ca42fc4224efa43c0a9e77
ikoww27 <ikowardhana98@gmail.com>
2025-01-20 17:44:37 +0700

    deployment

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+// Check if we're running on Vercel
+if (getenv('VERCEL')) {
+    // SSL Certificate path
+    putenv('SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt');
+}
+
 require __DIR__ . '/../public/index.php';
